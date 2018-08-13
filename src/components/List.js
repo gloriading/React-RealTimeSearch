5a302135e61c1f689d2601023c8e87d0932ed7e0
Gloria Ding <gloria.hc.ding@gmail.com>
2018-08-12 19:16:22 -0700

    Fetch data from public api

--- src/components/List.js
+++ src/components/List.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react';
 
-const boys = [
-  { name: "Alex", age: 2 },
-  { name: "Benny", age: 5 },
-  { name: "Calvin", age: 3 },
-  { name: "Dominion", age: 2 },
-  { name: "Elvis", age: 4 },
-];
-
 export default class List extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      people: [],
+      isLoad: true
+     }
+  }
+
+  componentDidMount(){
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('http://uinames.com/api/?amount=25&gender=female&region=canada')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        people: data,
+        isLoad: false
+       })
+    })
   }
 
   render(){
     let list, filteredList;
-    filteredList = boys.filter(boy => {
-      return boy.name.toLowerCase().indexOf(this.props.searchTerm) != -1
-    })
-    
-    list = filteredList.map( boy => {
-      return <li key={boy.name}>{boy.name} - {boy.age} years old</li> ;
+    filteredList = this.state.people.filter(person => {
+      return person.name.toLowerCase().indexOf(this.props.searchTerm) !== -1
     })
 
-    return(
-      <div className="list-container">
-        <ul>In List</ul>
-        {list}
-      </div>
-    )
+    list = filteredList.map( person => {
+      return <li key={person.id}>{person.name}</li> ;
+    })
+
+    if(this.state.isLoad){
+      return <div className="list-container">Loading...</div>;
+    }else{
+      return(
+        <div className="list-container">
+          <ul>
+          {list}
+          </ul>
+        </div>
+      );
+    }
   }
 }
